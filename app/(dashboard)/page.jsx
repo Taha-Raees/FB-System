48445b83b75c67a0d14cfb68802e4d9d0d000e9b
Muhammad Taha Raees <raeesmuhammadtaha@gmail.com>
2024-09-13 22:25:21 +0100

    fix routing and redux

--- app/(dashboard)/page.jsx
+++ app/(dashboard)/page.jsx
@@ -1,18 +1,24 @@
 "use client";
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import Loading from '@/Components/ui/Loading/Loading';
 import DashboardContent from '@/Components/DashboardItem/DashboardContent/DashboardContent';
-const Home = () => {
-  
 
-  return (
-   
-      
-      <DashboardContent/>
-      
-    
-    ); 
+const Home = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const checkSignIn = () => {
+      const isSignedIn = localStorage.getItem('isSignedIn');
+      if (isSignedIn !== 'true') {
+        router.push('/getstarted'); // Redirect to get started page if not signed in
+      }
+    };
+
+    checkSignIn();
+  }, [router]);
+
+  return <DashboardContent/>; 
 };
 
 export default Home;
